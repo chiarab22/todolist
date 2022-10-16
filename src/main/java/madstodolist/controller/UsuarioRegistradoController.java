4c83377d2462d85ac6fe8f5b821a98a594a92fb4
Chiara Boccaletti <cbocc@myuax.com>
2022-10-16 08:11:17 +0200

    usuarios Registrados

--- src/main/java/madstodolist/controller/UsuarioRegistradoController.java
+++ src/main/java/madstodolist/controller/UsuarioRegistradoController.java
@@ -30,68 +30,22 @@ public class UsuarioRegistradoController {
     UsuarioRepository usuarioRepository;
 
     @GetMapping("/registrados")
-    public
-    String usuarioRegistrado(Model model){
+    public String usuarioRegistrado(Model model) {
         List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
         model.addAttribute("usuario", usuarios);
         return "usuariosRegistrados";
     }
+
+
+
+    @GetMapping("/usuarios/:{id}")
+    public String UsuarioRegistradoById(@PathVariable(value="id") Long idUsuario, Model model){
+    Usuario usuarioId = usuarioService.findById(idUsuario);
+    if(usuarioId!= null){
+        return "descripcionUsuario";
     }
-
-    @PostMapping("/usuarios/{id}/tareas/nueva")
-    public String nuevaTarea(@PathVariable(value="id") Long idUsuario, @ModelAttribute TareaData tareaData,
-                             Model model, RedirectAttributes flash,
-                             HttpSession session) {
-
-        comprobarUsuarioLogeado(idUsuario);
-
-        Usuario usuario = usuarioService.findById(idUsuario);
-        tareaService.nuevaTareaUsuario(idUsuario, tareaData.getTitulo());
-        flash.addFlashAttribute("mensaje", "Tarea creada correctamente");
-        return "redirect:/usuarios/" + idUsuario + "/tareas";
+    throw new UsuarioNoLogeadoException();
     }
+}
 
-    @GetMapping("/usuarios/{id}/tareas")
-    public String listadoTareas(@PathVariable(value="id") Long idUsuario, Model model, HttpSession session) {
 
-        comprobarUsuarioLogeado(idUsuario);
-
-        Usuario usuario = usuarioService.findById(idUsuario);
-        List<Tarea> tareas = tareaService.allTareasUsuario(idUsuario);
-        model.addAttribute("usuario", usuario);
-        model.addAttribute("tareas", tareas);
-        return "listaTareas";
-    }
-
-    @GetMapping("/tareas/{id}/editar")
-    public String formEditaTarea(@PathVariable(value="id") Long idTarea, @ModelAttribute TareaData tareaData,
-                                 Model model, HttpSession session) {
-
-        Tarea tarea = tareaService.findById(idTarea);
-        if (tarea == null) {
-            throw new TareaNotFoundException();
-        }
-
-        comprobarUsuarioLogeado(tarea.getUsuario().getId());
-
-        model.addAttribute("tarea", tarea);
-        tareaData.setTitulo(tarea.getTitulo());
-        return "formEditarTarea";
-    }
-
-    @PostMapping("/tareas/{id}/editar")
-    public String grabaTareaModificada(@PathVariable(value="id") Long idTarea, @ModelAttribute TareaData tareaData,
-                                       Model model, RedirectAttributes flash, HttpSession session) {
-        Tarea tarea = tareaService.findById(idTarea);
-        if (tarea == null) {
-            throw new TareaNotFoundException();
-        }
-
-        Long idUsuario = tarea.getUsuario().getId();
-
-        comprobarUsuarioLogeado(idUsuario);
-
-        tareaService.modificaTarea(idTarea, tareaData.getTitulo());
-        flash.addFlashAttribute("mensaje", "Tarea modificada correctamente");
-        return "redirect:/usuarios/" + tarea.getUsuario().getId() + "/tareas";
-    }
